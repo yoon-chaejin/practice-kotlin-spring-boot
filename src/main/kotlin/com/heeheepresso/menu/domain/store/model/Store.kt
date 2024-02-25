fc3140e7678d0fe2efab7ac27eb33d68dd64c112
yoon-chaejin <kevin3st@naver.com>
2024-02-25 22:02:23 +0900

    [Menu] feat(Store): 매장 class 추가

--- src/main/kotlin/com/heeheepresso/menu/domain/store/model/Store.kt
+++ src/main/kotlin/com/heeheepresso/menu/domain/store/model/Store.kt
@@ -1,3 +1,27 @@
 package com.heeheepresso.menu.domain.store.model
 
-data class Store()
+import java.time.LocalTime
+
+class Store(
+        val id: Long?,
+        val name: String,
+        val hasDecaffeinatedMenu: Boolean,
+        val hasGrilledMenu: Boolean,
+        val isDeliveryAvailable: Boolean,
+        val isDirectManagedStore: Boolean,
+        val thumbnailUrl: String,
+        val address: Address,
+        val openingHours: List<OpeningHour>
+)
+
+data class Address(
+        val roadName: String,
+        val detail: String,
+        val longitude: Float,
+        val latitude: Float,
+)
+
+data class OpeningHour(
+        val startTime: LocalTime,
+        val endTime: LocalTime,
+)
